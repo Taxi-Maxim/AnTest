ce2622376f7a85441fe15c9b5666c402eac86f5f
Taxi-Maxim <151550640+Taxi-Maxim@users.noreply.github.com>
2025-06-01 19:29:47 +0300

    Add files via upload

---
+++ AnTest.Tests/EmailProcessingTests.cs
@@ -0,0 +1,70 @@
+﻿using AnTest.Models;
+using AnTest.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+
+
+namespace AnTest.Tests
+{
+    [TestClass]
+    public class EmailProcessingTests
+    {
+        [TestMethod]
+        public void ProcessEmailCopy_AddsAddress_WhenBusinessDomainExists()
+        {
+            var email = new EmailModel
+            {
+                To = "q.qweshnikov@batut.com; w.petrov@alfa.com;",
+                Copy = "f.patit@buisness.com"
+            };
+
+            var result = EmailProcessing.ProcessEmailCopy(email);
+
+            Assert.AreEqual("f.patit@buisness.com;v.vladislavovich@alfa.com;", result);
+        }
+
+        [TestMethod]
+        public void ProcessEmailCopy_DoesNotAddAddress_WhenExceptionExists()
+        {
+            var email = new EmailModel
+            {
+                To = "t.kogni@acl.com",
+                Copy = "i.ivanov@tbank.ru"
+            };
+
+            var result = EmailProcessing.ProcessEmailCopy(email);
+
+            Assert.AreEqual("i.ivanov@tbank.ru;", result);
+        }
+
+        [TestMethod]
+        public void ProcessEmailCopy_RemovesAddresses_WhenExceptionExists()
+        {
+            var email = new EmailModel
+            {
+                To = "t.kogni@acl.com; i.ivanov@tbank.ru;",
+                Copy = "e.gras@tbank.ru; t.tbankovich@tbank.ru; v.veronickovna@tbank.ru;"
+            };
+
+            var result = EmailProcessing.ProcessEmailCopy(email);
+
+            Assert.AreEqual("e.gras@tbank.ru;", result);
+        }
+
+        [TestMethod]
+        public void ProcessEmailCopy_DoesNotChange_WhenNoBusinessDomains()
+        {
+            var email = new EmailModel
+            {
+                To = "z.xcy@email.com",
+                Copy = "p.rivet@email.com"
+            };
+
+            var result = EmailProcessing.ProcessEmailCopy(email);
+
+            Assert.AreEqual("p.rivet@email.com;", result);
+        }
+    }
+}
---
+++ AnTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnTest.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c96d945a-8199-4127-8c8a-92eb0c3e6116")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
